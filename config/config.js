39505502282a584503ad6311570fc0ff31cc3a4b
abhijit ghanta <abhijit.ghanta@klizos.com>
2025-01-17 19:07:10 +0530

    hotfix

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
-export let allowOrigins = ["http://localhost:3000", "http://localhost:4000"];
+export let allowOrigins = [
+  "http://localhost:3000",
+  "http://localhost:4000",
+  "https://ecommerce-shop332.netlify.app/",
+];
